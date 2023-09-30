4182a65487449cc48955094a5bdf47bf39b6dd65
Berkay <92801714+Bechoarslan@users.noreply.github.com>
2023-10-01 01:14:36 +0300

    Initial commit

---
+++ Assets/Scripts/Runtime/Signals/UISignals.cs
@@ -0,0 +1,13 @@
+
+using Runtime.Extentions;
+using UnityEngine.Events;
+
+namespace Runtime.Signals
+{
+    public class UISignals : MonoSingleton<UISignals>
+    {
+        public UnityAction onUpdateThrowableCount = delegate { };
+        public UnityAction onUpdateLeftEnemyCount = delegate { };
+        public UnityAction<int> onSetNewLevelValue = delegate { };
+    }
+}
