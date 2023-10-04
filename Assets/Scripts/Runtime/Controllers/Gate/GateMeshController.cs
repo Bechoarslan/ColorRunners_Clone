998fc3bbc4b78d0e828f0563051850243a5f19a4
Berkay <barslann09@gmail.com>
2023-10-04 06:32:01 +0300

    Gate Manager commit

--- Assets/Scripts/Runtime/Controllers/Gate/GateMeshController.cs
+++ Assets/Scripts/Runtime/Controllers/Gate/GateMeshController.cs
@@ -1,38 +1,9 @@
-using System;
-using Runtime.Signals;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Runtime.Controllers.Gate
 {
     public class GateMeshController : MonoBehaviour
     {
-        #region Self Variables
-
-        #region Private Variables
-
-        private new Renderer _gateRenderer;
-
-        #endregion
-        #endregion
-
-        private void Awake()
-        {
-            GetReferences();
-        }
-
-        private void GetReferences()
-        {
-            _gateRenderer = GetComponent<Renderer>();
-        }
-
-        private void OnEnable()
-        {
-            GateSignals.Instance.onSetGateColor += OnSetGateColor;
-        }
-
-        private void OnSetGateColor(Color32 value)
-        {
-            _gateRenderer.material.color = value;
-        }
+        
     }
 }
